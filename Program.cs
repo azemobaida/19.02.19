455b1c290b2a5c09939e4a93ef5004c61734ce32
azemobaida <47791218+azemobaida@users.noreply.github.com>
2019-02-19 19:46:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.Write("enter number");
+
+			int a = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("enter number");
+
+			int b = Convert.ToInt32(Console.ReadLine());
+
+			if (a > b)
+			{
+				// a is bigger
+				Console.WriteLine(a);
+
+			}
+			else
+			{
+				// b is bigger
+				Console.WriteLine(b);
+			}
+		}
+	}
+}
